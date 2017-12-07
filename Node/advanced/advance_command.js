587b0e3df62372a5648c5967dd40b394662a06ce
fuzongjian <2917066078@qq.com>
2017-12-07 17:43:19 +0800

    log4js库的测试应用

--- Node/advanced/advance_command.js
+++ Node/advanced/advance_command.js
@@ -3,4 +3,6 @@ const router = express.Router();
 router.get('/',function (req,res) {
 	res.send('we will start to study advanced node');
 });
+const log = require('./log');
+router.get('/log',log);
 module.exports = router;

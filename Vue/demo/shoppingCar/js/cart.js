6bebd0c0ee3cb0603d9713d3c6e6840af9e644da
fuzongjian <2917066078@qq.com>
2017-12-13 20:49:50 +0800

    网站设计demo

--- Vue/demo/shoppingCar/js/cart.js
+++ Vue/demo/shoppingCar/js/cart.js
@@ -12,9 +12,9 @@ var vm = new Vue({
 	},
 	methods:{
 		cartView:function () {
-			this.$http.get("http://www.baidu.com").then(function (res) {
-				console.log(res.body)
-			});
+			this.$http.get("www.baidu.com").then(res=>{
+				console.log('hello world')
+			})
 		}
 	}
 })
---
+++ Vue/demo/webdesign/src/components/layout.vue
@@ -0,0 +1,150 @@
+<template>
+	<div>
+		<!-- head -->
+		<div class="app-head">
+			<div class="app-head-inner">
+				<img src="../assets/logo.png">
+				<div class="head-nav">
+					<ul class="nav-list">
+						<li>{{ username }}</li>
+						<li class="nav-pile">|</li>
+						<li  @click="quit">退出</li>
+						<li   @click="logClick">登录</li>
+						<li class="nav-pile">|</li>
+						<li  @click="regClick">注册</li>
+			            <li  class="nav-pile">|</li>
+			            <li @click="aboutClick">关于</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- container -->
+		<div class="container">container</div>
+		<!-- footer -->
+		<div class="app-foot">
+			<p>© 2017 www.github.com</p>
+		</div>
+	</div>
+</template>
+<script>
+export default{
+	components:{
+
+	},
+	data(){
+		return{
+			username:''
+		}
+	},
+	methods:{
+		quit(){
+
+		},
+		logClick(){
+
+		},
+		regClick(){
+
+		},
+		aboutClick(){
+
+		}
+	}
+}
+</script>
+<style>
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body{
+	line-height: 1;
+}
+ol,ul{
+	list-style: none;
+}
+blockquote,q{
+	quotes: none;
+}
+blockquote:before,blockquote:after,
+q:before,q:after{
+	content: '';
+	content: none;
+}
+table{
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+a{
+	color: inherit;
+	text-decoration: none;
+}
+body{
+	background: #f0f2f5;
+	font-family: "Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+	font-size: 14px;
+	color: #444;
+}
+.app-head{
+	background: #363636;
+	color: #b2b2b2;
+	height: 90px;
+	line-height: 90px;
+	width: 100%;
+}
+.app-header-inner{
+	width: 1200px;
+	margin: 0 auto;
+}
+.app-head-inner img{
+	width: 50px;
+	margin-top: 20px;
+}
+.head-nav{
+	float: right;
+}
+.head-nav ul{
+	overflow: hidden;
+}
+.head-nav li{
+	cursor: pointer;
+	float: left;
+}
+.nav-pile{
+	padding: 0 15px;
+}
+.app-foot{
+	text-align: center;
+	height: 80px;
+	width: 100%;
+	line-height: 80px;
+	background: #e3e4e8;
+	clear: both;
+	margin-top: 30px;
+}
+</style>

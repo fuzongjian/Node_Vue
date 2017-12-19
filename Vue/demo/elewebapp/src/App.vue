b10ce584d8f0858113af17b08123ee05eda4848f
fuzongjian <2917066078@qq.com>
2017-12-19 19:13:11 +0800

    界面的书写及其逻辑处理

--- Vue/demo/elewebapp/src/App.vue
+++ Vue/demo/elewebapp/src/App.vue
@@ -42,27 +42,23 @@ export default {
 }
 </script>
 
-<style>
-  /*@import "./common/stylus/mixin.styl"*/
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab 
+    display flex 
+    width 100%
+    height 40px
+    line-height 40px
+    border-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex 1 
+      text-align center
+      & > a
+        display block
+        font-size 14px
+        color rgb(77,85,93)
+        &.active
+          color rgb(240,20,20)
+        
 
-#app .tab{
-  display: flex;
-  width: 100%;
-  height: 40px;
-  line-height: 40px;
-  /*border-1px(rgba(7, 17, 27, 0.1))*/
-  border:1px solid rgba(7, 17, 27, 0.1);
-}
-#app .tab .tab-item{
-  flex: 1;
-  text-align: center;
-}
-#app .tab .tab-item a{
-  display: block;
-  font: 14px;
-  color: rgb(77, 85, 93);
-}
-#app .tab .tab-item a:hover{
-  color: rgb(240, 20, 20);
-}
 </style>
